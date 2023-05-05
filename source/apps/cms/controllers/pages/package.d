09840cbe696180e0dd2ba37330484e4282b88951
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-05 18:19:35 +0200

    Update

--- source/apps/cms/controllers/pages/package.d
+++ source/apps/cms/controllers/pages/package.d
@@ -1,1 +1,1 @@
-
+module apps.cms.controllers.pages;
--- source/apps/cms/package.d
+++ source/apps/cms/package.d
@@ -1,1 +1,37 @@
 module apps.cms;
+
+mixin(ImportPhobos!());
+
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import langs.javascript;
+public import uim.html;
+// public import uim.mongo;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+// uim-cms
+/* public import apps.cms.controllers;
+public import apps.cms.data;
+public import apps.cms.helpers;
+public import apps.cms.interfaces;
+public import apps.cms.javascripts;
+public import apps.cms.layouts;
+public import apps.cms.scripts;
+public import apps.cms.settings;
+public import apps.cms.translations;
+public import apps.cms.views; */
+
+public {
+  import apps.cms.controllers;
+  import apps.cms.helpers;
+  import apps.cms.tests;
+  import apps.cms.views;
+}

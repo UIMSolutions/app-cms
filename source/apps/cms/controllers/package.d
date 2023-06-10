664502b8c7f87d100fa5fa75e6606236701b7927
UIMSolutions <ui.manufaktur@gmail.com>
2023-06-10 13:58:02 +0200

    Updates README

--- source/apps/cms/controllers/package.d
+++ source/apps/cms/controllers/package.d
@@ -5,3 +5,13 @@ public {
   import apps.cms.controllers.components;
   import apps.cms.controllers.pages;
 }
+
+public {
+  import apps.cms.controllers.controller;
+  import apps.cms.controllers.create;
+  import apps.cms.controllers.delete_;
+  import apps.cms.controllers.error;
+  import apps.cms.controllers.index;
+  import apps.cms.controllers.read;
+  import apps.cms.controllers.update;
+}
